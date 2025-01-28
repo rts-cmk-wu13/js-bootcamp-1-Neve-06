211ed8225d473105e0602201780c1b0b84940216
Neve-06 <6255234@rts-365.dk>
2025-01-28 10:21:39 +0100

    added forEach function. In the console, it says all the quotes

--- quotes.js
+++ quotes.js
@@ -1,1 +1,7 @@
 let quotes = ["Do you like scary movies?", "Here's Johnny!", "Was that the boogeyman?", "Be afraid, Be very afraid", "Whatever you do, don't fall asleep", "You'll float too", "You're gonna need a bigger boat", "Where is everyone going? Bingo?", "In my restless dreams, i see that town. Silent hill", "Hi i am Chucky, your friend till the end"]
+let quotesElm = document.querySelector("blockquote")
+
+quotes.forEach(function(quote){
+    console.log(quote);
+    quotesElm += `<blockquote>${quote}</blockquote>`
+})
