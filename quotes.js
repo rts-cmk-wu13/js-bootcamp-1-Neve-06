6c7a8c60cbe35b57607d1a7c39e575a36cf456d7
Neve-06 <6255234@rts-365.dk>
2025-01-28 10:14:38 +0100

    added quotes

---
+++ quotes.js
@@ -0,0 +1,1 @@
+let quotes = ["Do you like scary movies?", "Here's Johnny!", "Was that the boogeyman?", "Be afraid, Be very afraid", "Whatever you do, don't fall asleep", "You'll float too", "You're gonna need a bigger boat", "Where is everyone going? Bingo?", "In my restless dreams, i see that town. Silent hill", "Hi i am Chucky, your friend till the end"]
